4e2b51b71e40c3c7c0afa971ca3459780d60adfb
Maxxxing <mingma0626@gmail.com>
2022-11-27 16:54:17 -0800

    1

--- grade.sh
+++ grade.sh
@@ -4,6 +4,39 @@ rm -rf student-submission
 git clone $1 student-submission
 echo 'Finished cloning'
 
+cp TestListExamples.java student-submission
+cp -r lib student-submission
 
+cd student-submission
 
+if [[ -e ListExamples.java ]]
+then 
+    echo "file found"
+else 
+    echo "file not found"
+    exit 1
+fi 
 
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java &> compile.txt 
+
+if [ $? -eq 0 ]
+then 
+    echo "passed"
+    java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples &> result.txt
+
+    if grep "OK" result.txt
+    then 
+        echo "Test passed"
+    fi
+
+    if grep "FAILURES" result.txt
+    then 
+        echo "Failed"
+    fi
+    
+else
+    cat comple.txt
+    echo "Compile error"
+    echo "Failed"
+    exit 1
+fi
